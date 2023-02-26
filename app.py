30fd52d242a1f290c5ba5708e54dba014c4198a0
Emad Ali <98942435+qarisaab@users.noreply.github.com>
2023-02-26 13:56:43 +0000

    full

---
+++ app.py
@@ -0,0 +1,44 @@
+import string
+import random
+
+def generate_password(min_length, numbers=True, special_character=True):
+    letters= string.ascii_letters
+    number = string.digits
+    special = string.punctuation
+
+    characters = letters
+
+    if number:
+        characters += number
+
+    if special:
+        characters += special
+
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in number:
+            has_number = True
+
+        if new_char in special:
+            has_special = True
+
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_character:
+            meets_criteria = meets_criteria and has_special
+
+
+    return pwd
+
+
+pwd=generate_password(10)
+print(pwd)
+
